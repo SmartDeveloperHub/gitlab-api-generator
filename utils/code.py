7b1097b562521bc30432369cb69afd747d91d2a1
Alejandro Fdez Carrera <alej4fc@gmail.com>
2015-10-01 11:19:16 +0200

    move and generate python code

--- utils/code.py
+++ utils/code.py
@@ -25,8 +25,30 @@ import json
 __author__ = 'Alejandro F. Carrera'
 
 
-def generate_python_api(metadata):
+def save_json_metadata(metadata):
     settings.print_message(" - Saving metadata at json format ... ")
     f = open("generated/metadata.json", "w")
     f.write(json.dumps(metadata))
     f.close()
+
+
+def generate_pycode():
+    settings.print_message(" - Generating Python code ... ")
+    f = open("generated/template.tmp", "r")
+    t_file = f.read()
+    f.close()
+    f = open("generated/glapi.py", "w")
+    f.write(t_file)
+    f.close()
+
+
+def generate_pypi(version):
+    settings.print_message(" - Generating Pypi: %s ... " % version)
+    f = open("generated/settings.tmp", "r")
+    t_settings = f.read()
+    f.close()
+    t_settings = t_settings.replace("API_VERSION_TEMPLATE", version)
+    f = open("generated/settings.py", "w")
+    f.write(t_settings)
+    f.close()
+
